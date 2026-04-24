a2fe6f22c8aad3712014c1e7701f57b06c8efcd4
SimonM32 <simi1002@stud.ek.dk>
2026-04-24 13:40:58 +0200

    CircleClass feature

--- src/main/java/ShapeCalculator.java
+++ src/main/java/ShapeCalculator.java
@@ -1,12 +1,5 @@
 public class ShapeCalculator {
 
-    public double calculateCircleArea(double radius) {
-        return Math.PI * radius * radius;
-    }
-
-    public double calculateCircleCircumference(double radius) {
-        return 2 * Math.PI * radius;
-    }
 
     public double calculateRectangleArea(double width, double height) {
         return width * height;
