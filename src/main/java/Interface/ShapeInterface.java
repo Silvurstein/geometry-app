0d3c16f183015988c4ef702cbc8c636cec91a14f
silvurstein <sveinursilvurstein@gmail.com>
2026-04-24 13:45:31 +0200

    sveinur er et genius

---
+++ src/main/java/Interface/ShapeInterface.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public interface ShapeInterface {
+    void calculateCircleArea(double radius);
+
+    void calculateCircleCircumference(double radius);
+
+    void calculateRectangleArea(double width, double height);
+
+    void calculateRectanglePerimeter(double width, double height);
+}
+
